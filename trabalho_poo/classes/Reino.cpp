2af5e37beff8b8a93dea7f92dc662f4cd5f75993
Júlia Freitas <juliafreitas@alu.ufc.br>
2022-12-03 15:53:14 -0300

    Objetos do Reino

--- trabalho_poo/classes/Reino.cpp
+++ trabalho_poo/classes/Reino.cpp
@@ -3,19 +3,17 @@
 #include <string>
 using namespace std;
 
-void Reino::definirNomeReino(string nome){
+void Reino::definirNomeReino(string nome)
+{
     NomeReino = nome;
 }
 
-void Reino::CriaReino(){
-    Reino Reino1;
+void Reino::CriaReino()
+{
+    Reino Reino1, Reino2, Reino3, Reino4, Reino5;
     Reino1.definirNomeReino("Animalia");
-    Reino Reino2;
     Reino2.definirNomeReino("Fungi");
-    Reino Reino3;
     Reino3.definirNomeReino("Plantae");
-    Reino Reino4;
     Reino4.definirNomeReino("Protista");
-    Reino Reino5;
     Reino5.definirNomeReino("Monera");
 }
