80c9f4d18be62aedebf0208a4348a075538bed2d
contrasov <agvdsbs@gmail.com>
2022-12-03 13:47:28 -0300

    opa
    c
    sadas

---
+++ trabalho_poo/classes/Reino.hpp
@@ -0,0 +1,10 @@
+//classe reino 
+#include <iostream>
+#include <string>
+using namespace std;
+class Reino{
+    private:
+        string NomeReino;
+    public:
+        string getNomeReino();
+};
